60886068668c6befdb2529e975bd7f66ff5619f9
m4nt3k1ll4 <juanchin23ad@gmail.com>
2026-04-10 09:40:26 -0500

    Fix logo dimensions in email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('LOGO-04-1.png') }}" style="max-height: 55px; max-width: 220px;" alt="{{ config('app.name') }}">
+<img src="{{ asset('LOGO-04-1.png') }}" style="max-height: 5px; max-width: 22px;" alt="{{ config('app.name') }}">
 </a>
 </td>
 </tr>
